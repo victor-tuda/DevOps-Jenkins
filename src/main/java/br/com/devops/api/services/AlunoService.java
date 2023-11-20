3ca597f3a89af45770f4c99f8162a8c2f11e0bee
Victor Tuda <victor_gtuda@outlook.com>
2023-11-20 17:32:12 -0300

    feat: alterando a interface para conter o método findAll

--- src/main/java/br/com/devops/api/services/AlunoService.java
+++ src/main/java/br/com/devops/api/services/AlunoService.java
@@ -2,6 +2,10 @@ package br.com.devops.api.services;
 
 import br.com.devops.api.domain.Aluno;
 
+import java.util.List;
+
 public interface AlunoService {
     Aluno findById(Integer id );
+    List<Aluno> findAll();
+
 }
